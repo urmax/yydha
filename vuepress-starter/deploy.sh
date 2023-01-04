9a038a6e8d05632eead81d83ec418b90737d27b0
tanyaohua <1361827298@qq.com>
2023-01-04 17:51:08 +0800

    提交

---
+++ vuepress-starter/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd ../.vuepress/dist
+
+# 拷贝目录和文件
+cp -r ../../.github ./
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:urmax/yydha.git master:gh-pages
+
+cd -
